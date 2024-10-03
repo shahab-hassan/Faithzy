3ded2c6df9a35b1b4210a2862901a5f9566522ce
Shahab Hassan <shahabspeaks567@gmail.com>
2024-10-03 21:01:46 +0500

    Frontend Deployed

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,5 +1,6 @@
 // const hostNameFront = "http://localhost:3000";
 // const hostNameFront = "https://faithzy-shahab-hassans-projects.vercel.app";
-const hostNameFront = "https://faithzy.vercel.app";
+// const hostNameFront = "https://faithzy.vercel.app";
+const hostNameFront = "https://faithzy.onrender.com/";
 
 module.exports = {hostNameFront};
