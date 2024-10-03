0e4e21c63b992b6d41009098b0685b627dbe74d6
Shahab Hassan <shahabspeaks567@gmail.com>
2024-10-03 10:35:03 +0500

    Fixed Deployment Bugs

--- frontend/src/components/buyer/Hero.js
+++ frontend/src/components/buyer/Hero.js
@@ -9,7 +9,7 @@ function Hero() {
                 <div className="heroContent">
 
                     <div className="heroContentMain">
-                        <h1>EMPOWER YOUR <span>FAITH</span></h1>
+                        <h1>EMPOWER <span>FAITH</span></h1>
                         <p>Explore a vast selection of religious products and services from trusted sellers. Whether seeking spiritual guidance, artifacts, or events, we've got you covered.</p>
                         <div className="heroBtns">
                             <Link to="/products" className='luxuryBtn'>Explore Products <i className="fa-solid fa-arrow-right"></i></Link>
